ced4b5b2a58644d7e52698aa4508db13108469ac
Suraj <surajybtg@gmail.com>
2023-01-06 11:21:40 +0530

    API working

--- netflix-server/routes/UserRoutes.js
+++ netflix-server/routes/UserRoutes.js
@@ -6,6 +6,9 @@ const {
 
 const router = require("express").Router();
 
+router.get("/", (req, res) => {
+  res.json({ status: successfull });
+});
 router.get("/liked/:email", getLikedMovies);
 router.post("/add", addToLikedMovies);
 router.put("/remove", removeFromLikedMovies);
